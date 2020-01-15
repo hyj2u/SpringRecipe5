dee465a8b20a7f892c47bdc86c8ed20e74882ba7
hyj2u <43366665+hyj2u@users.noreply.github.com>
2020-01-15 11:55:40 +0900

    자바로 POJO 구성하기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
+import dao.SequenceDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class);
-        SequenceGenerator generator = context.getBean(SequenceGenerator.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext("dao");
+        SequenceDao sequenceDao = context.getBean(SequenceDao.class);
 
-        System.out.println(generator.getSequence());
-        System.out.println(generator.getSequence());
+        System.out.println(sequenceDao.getNextValue("IT"));
+        System.out.println(sequenceDao.getNextValue("IT"));
     }
 }
